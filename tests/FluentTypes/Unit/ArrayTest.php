414863ab94a3444fbe23f381f0e4eaa003c48bde
Josh Bruce <josh@8fold.pro>
2020-08-24 21:03:53 -0500

    separate unit test setup into abstract class

--- tests/FluentTypes/Unit/ArrayTest.php
+++ tests/FluentTypes/Unit/ArrayTest.php
@@ -1,12 +1,9 @@
 <?php
 
-namespace Eightfold\Shoop\FluentTypes\Tests\Unit;
+namespace Eightfold\Shoop\Tests\FluentTypes\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Eightfold\Shoop\Tests\FluentTypes\Unit\UnitTestCase;
 use Eightfold\Shoop\Tests\AssertEqualsFluent;
-
-use \ReflectionClass;
-use \ReflectionMethod;
 
 use Eightfold\Shoop\Shoop;
 
@@ -21,8 +18,13 @@ use Eightfold\Shoop\FluentTypes\ESTuple;
 /**
  * @group ArrayFluentUnit
  */
-class ArrayTest extends TestCase
+class ArrayTest extends UnitTestCase
 {
+    static public function sutClassName(): string
+    {
+        return ESArray::class;
+    }
+
     /**
      * @test
      */
@@ -119,75 +121,5 @@ class ArrayTest extends TestCase
         )->unfoldUsing(
             Shoop::this(["a", "b"])->asTuple()
         );
-    }
-
-    public function setUp(): void
-    {
-        $this->caseExistForEachMethod();
-    }
-
-    public function caseExistForEachMethod()
-    {
-        $caseMethods = array_map(
-            function($reflectionMethod) {
-                if (! in_array($reflectionMethod->name, ["setUp", "testsExistForEachMethod"]) and
-                    $reflectionMethod->class === static::class
-                ) {
-                    return $reflectionMethod->name;
-                }
-            },
-            (new ReflectionClass(static::class))->getMethods(ReflectionMethod::IS_PUBLIC),
-        );
-        $caseMethods = array_values(array_filter($caseMethods));
-
-        $sutMethods = array_map(
-            function($reflectionMethod) {
-                if (! in_array($reflectionMethod->name, $this->ignoreClassMethods()) and
-                    $reflectionMethod->name[0] !== "_"
-                ) {
-                    return $reflectionMethod->name;
-                }
-            },
-            (new ReflectionClass(ESArray::class))->getMethods(ReflectionMethod::IS_PUBLIC),
-        );
-        $sutMethods = array_values(array_filter($sutMethods));
-        $sutMethods[] = "php_iterator";
-
-        $notTested = array_diff($sutMethods, $caseMethods);
-        sort($notTested);
-        $notTestedString = print_r($notTested, true);
-        $this->assertEquals(0, count($notTested), "The following methods have not been tested (only whether a test method exists): {$notTestedString}");
-    }
-
-    /**
-     * Ignore these methods because they are either value holders, use the basic
-     * implementation from Shoop pipes, is covered by another set of test cases,
-     * or some combination thereof.
-     */
-    private function ignoreClassMethods()
-    {
-        return [
-            "args",           // value method, returns args following or incl. main
-            "efToArray",      // uses Shoop default (asArray)
-            "efToBoolean",    // uses Shoop default (asBoolean)
-            "efToDictionary", // uses Shoop default (asDictionary)
-            "efToInteger",    // uses Shoop default (asInteger)
-            "efToJson",       // uses Shoop default (asJson)
-            "efToString",     // uses Shoop default (asJson)
-            "efToTuple",      // uses Shoop default (asJson)
-            "fold",           // uses Foldable default
-            "count",          // uses Shoop default (efToInteger)
-            "jsonSerialize",  // uses Shoop default (efToTuple)
-            "offsetExists",   // uses Shoop default (hasMember)
-            "offsetGet",      // uses Shoop default (at)
-            "offsetSet",      // uses Shoop default (plusMember ??) TODO: Should plus() be solely for values
-            "offsetUnset",    // uses Shoop default (minusMember)
-            "unfold",         // uses Foldable default
-            "rewind",         // part of php_iterator
-            "valid",          // part of php_iterator
-            "current",        // part of php_iterator
-            "key",            // part of php_iterator
-            "next",           // part of php_iterator
-        ];
     }
 }
