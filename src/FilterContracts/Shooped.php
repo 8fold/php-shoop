bcda937b48fc6e0da37d33cb2eb5265716f7ddb3
Josh Bruce <josh@8fold.pro>
2020-08-27 17:46:29 -0500

    remove deprecated interfaces

--- src/FilterContracts/Shooped.php
+++ src/FilterContracts/Shooped.php
@@ -4,7 +4,6 @@ namespace Eightfold\Shoop\FilterContracts;
 
 use Eightfold\Foldable\Foldable;
 
-// use Eightfold\Shoop\FilterContracts\Interfaces\Addable;
 use Eightfold\Shoop\FilterContracts\Interfaces\Arrayable;
 use Eightfold\Shoop\FilterContracts\Interfaces\Associable;
 use Eightfold\Shoop\FilterContracts\Interfaces\Comparable;
@@ -12,20 +11,17 @@ use Eightfold\Shoop\FilterContracts\Interfaces\Countable;
 use Eightfold\Shoop\FilterContracts\Interfaces\Falsifiable;
 use Eightfold\Shoop\FilterContracts\Interfaces\Reversible;
 use Eightfold\Shoop\FilterContracts\Interfaces\Stringable;
-// use Eightfold\Shoop\FilterContracts\Interfaces\Subtractable;
 use Eightfold\Shoop\FilterContracts\Interfaces\Tupleable;
 use Eightfold\Shoop\FilterContracts\Interfaces\Typeable;
 
 interface Shooped extends
     Foldable,
-    // Addable, part of Associable
     Arrayable,
     Comparable,
     Countable,
     Falsifiable,
     Reversible,
     Stringable,
-    // Subtractable, part of Associable
     Tupleable,
     Typeable
 {
