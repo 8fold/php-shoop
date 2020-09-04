ae4b4fc25d7b50a7b4b72e06cc9247f4a180b328
Josh Bruce <josh@8fold.pro>
2020-09-04 00:43:19 -0500

    Update Associable.php

--- tests/FilterContracts/ContractTests/Associable.php
+++ tests/FilterContracts/ContractTests/Associable.php
@@ -314,7 +314,7 @@ trait Associable
         AssertEqualsFluent::applyWith(
             true,
             Shooped::class,
-            0.52 // 0.5 // 0.46 // 0.4
+            0.53 // 0.52 // 0.5 // 0.46 // 0.4
         )->unfoldUsing(
             Shooped::fold(["a" => 1, "b" => 3, "c" => 1])->hasAt("c")
         );
@@ -393,7 +393,7 @@ trait Associable
         AssertEqualsFluent::applyWith(
             false,
             "boolean",
-            0.52 // 0.48
+            0.54 // 0.52 // 0.48
         )->unfoldUsing(
             Shooped::fold("Hi!")->offsetExists(4)
         );
