ddb28153ada5b3633c39ad2472b5efdaf8178b6a
Josh Bruce <josh@8fold.pro>
2020-09-25 18:07:55 -0500

    Subtractable tests passing - only used for numbers at present

--- src/Filter/Minus.php
+++ src/Filter/Minus.php
@@ -8,57 +8,34 @@ use Eightfold\Foldable\Filter;
 use Eightfold\Shoop\Shoop;
 
 /**
- * @todo - invocation, move type-specific methods to new Remove filter
+ * @version 1.0.0
  */
 class Minus extends Filter
 {
     public function __invoke($using)
     {
-        // if (TypeIs::applyWith("boolean")->unfoldUsing($using)) {
-        //     return Shoop::pipe($using,
-        //         TypeAsInteger::apply(),
-        //         Minus::applyWith($this->main),
-        //         TypeAsBoolean::apply()
-        //     )->unfold();
+        if (Is::object(false)->unfoldUsing($using)) {
+            // if (Is::object()->unfoldUsing($using)) {
+            //     return static::fromObject($using);
+            // }
+            // return static::fromTuple($using, $this->main);
 
-        // } elseif (TypeIs::applyWith("number")->unfoldUsing($using)) {
-        //     if (! is_array($this->main)) {
-        //         $this->main = [$this->main];
+        // } elseif (Is::boolean()->unfoldUsing($using)) {
+        //     return static::fromBoolean($using);
+
+        } elseif (Is::number()->unfoldUsing($using)) {
+            return static::fromNumber($using, ...$this->args(true));
+
+        // } elseif (Is::list()->unfoldUsing($using)) {
+        //     return static::fromList($using, $this->main);
+
+        // } elseif (Is::string()->unfoldUsing($using)) {
+        //     if (Is::json()->unfoldUsing($using)) {
+        //         return static::fromTuple($using, $this->main);
         //     }
+        //     return static::fromString($using, $this->main);
 
-        //     $int = array_sum($this->main);
-        //     return $using - $int;
-
-        // } elseif (TypeIs::applyWith("list")->unfoldUsing($using)) {
-        //     $base = $this->fromList($using, ...$this->args(true));
-        //     if (TypeIs::applyWith("array")->unfoldUsing($using)) {
-        //         return TypeAsArray::apply()->unfoldUsing($base);
-        //     }
-        //     return $base;
-
-        // } elseif (TypeIs::applyWith("string")->unfoldUsing($using) and
-        //     ! TypeIs::applyWith("json")->unfoldUsing($using)
-        // ) {
-        //     $args = $this->args(true);
-        //     return $this->fromString($using, ...$args);
-
-        // } elseif (TypeIs::applyWith("tuple")->unfoldUsing($using)) {
-        //     if (! is_array($this->main)) {
-        //         $this->main = [$this->main];
-        //     }
-        //     return Shoop::pipe($using,
-        //         TypeAsDictionary::apply(),
-        //         Minus::applyWith(...$this->args(true)),
-        //         TypeAsTuple::apply()
-        //     )->unfold();
-
-        // } elseif (TypeIs::applyWith("object")->unfoldUsing($using)) {
-        //     return Shoop::pipe($using,
-        //         TypeAsTuple::apply(),
-        //         Minus::applyWith(...$this->args(true))
-        //     )->unfold();
-
-        // }
+        }
     }
 
     // TODO: PHP 8 - int|float, int|float -> int|float
@@ -75,31 +52,31 @@ class Minus extends Filter
      *
      * TODO: PHP 8 - string, bool, bool, array|int
      */
-    private function fromString(
-        string $using,
-        array $charMask = [" ", "\t", "\n", "\r", "\0", "\x0B"],
-        bool $fromStart = true,
-        bool $fromEnd   = true
-    ): string
-    {
-        $fromStartAndEnd = ($fromStart and $fromEnd) ? true : false;
-        $all             = (! $fromStart and ! $fromEnd) ? true : false;
+    // private function fromString(
+    //     string $using,
+    //     array $charMask = [" ", "\t", "\n", "\r", "\0", "\x0B"],
+    //     bool $fromStart = true,
+    //     bool $fromEnd   = true
+    // ): string
+    // {
+    //     $fromStartAndEnd = ($fromStart and $fromEnd) ? true : false;
+    //     $all             = (! $fromStart and ! $fromEnd) ? true : false;
 
-        if ($fromStartAndEnd) {
-            $charMask = TypeAsString::apply()->unfoldUsing($charMask);
-            return trim($using, $charMask);
+    //     if ($fromStartAndEnd) {
+    //         $charMask = TypeAsString::apply()->unfoldUsing($charMask);
+    //         return trim($using, $charMask);
 
-        } elseif ($fromStart) {
-            $charMask = TypeAsString::apply()->unfoldUsing($charMask);
-            return ltrim($using, $charMask);
+    //     } elseif ($fromStart) {
+    //         $charMask = TypeAsString::apply()->unfoldUsing($charMask);
+    //         return ltrim($using, $charMask);
 
-        } elseif ($fromEnd) {
-            $charMask = TypeAsString::apply()->unfoldUsing($charMask);
-            return rtrim($using, $charMask);
+    //     } elseif ($fromEnd) {
+    //         $charMask = TypeAsString::apply()->unfoldUsing($charMask);
+    //         return rtrim($using, $charMask);
 
-        } elseif ($all) {
-            return str_replace($charMask, "", $using);
+    //     } elseif ($all) {
+    //         return str_replace($charMask, "", $using);
 
-        }
-    }
+    //     }
+    // }
 }
