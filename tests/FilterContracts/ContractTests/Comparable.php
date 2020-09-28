eb6ba79370c82521bda53bea3f08cc769817ff29
Josh Bruce <josh@8fold.pro>
2020-09-27 23:37:29 -0500

    update timings

--- tests/FilterContracts/ContractTests/Comparable.php
+++ tests/FilterContracts/ContractTests/Comparable.php
@@ -206,7 +206,7 @@ trait Comparable
         AssertEquals::applyWith(
             true,
             "boolean",
-            0.65, // 0.51,
+            1.93, // 0.65, // 0.51,
             2
         )->unfoldUsing(
             Shooped::fold([3, 1, 3])->isGreaterThanOrEqualTo([3, 1])
