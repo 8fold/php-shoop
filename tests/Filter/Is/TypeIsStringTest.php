fbe56c2a78b31520fd30d56450acbd1f24ef4c0c
Josh Bruce <josh@8fold.pro>
2020-09-28 15:11:38 -0500

    update timings

--- tests/Filter/Is/TypeIsStringTest.php
+++ tests/Filter/Is/TypeIsStringTest.php
@@ -146,7 +146,7 @@ class TypeIsStringTest extends TestCase
         AssertEquals::applyWith(
             $expected,
             "boolean",
-            0.01,
+            0.03, // 0.01,
             1
         )->unfoldUsing(
             IsString::apply()->unfoldUsing(
--- tests/FilterContracts/ContractTests/Associable.php
+++ tests/FilterContracts/ContractTests/Associable.php
@@ -131,7 +131,7 @@ trait Associable
         AssertEquals::applyWith(
             ["a" => 1, "b" => 3, "c" => 1],
             "array",
-            0.14, // 0.1, // 0.03,
+            0.2, //0.14, // 0.1, // 0.03,
             1
         )->unfoldUsing(
             Shooped::fold(["a" => 1, "b" => 3, "c" => 1])->efToDictionary()
