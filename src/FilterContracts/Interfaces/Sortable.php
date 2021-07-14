abf922dddf63d48f52b0b8bc461fbda32439f6bb
Josh Bruce <josh@8fold.pro>
2021-07-13 20:55:42 -0500

    php 7.4

--- src/FilterContracts/Interfaces/Sortable.php
+++ src/FilterContracts/Interfaces/Sortable.php
@@ -4,5 +4,6 @@ namespace Eightfold\Shoop\FilterContracts\Interfaces;
 
 interface Sortable
 {
-    public function sort(int|callable $flag = SORT_REGULAR): Sortable;
+    // public function sort(int|callable $flag = SORT_REGULAR): Sortable;
+    public function sort($flag = SORT_REGULAR): Sortable;
 }
